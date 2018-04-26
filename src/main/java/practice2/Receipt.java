c7370aa9c05670956cec3edd0a7c08c68b1a5c36
KomiyaYURI <879513168@qq.com>
2018-04-26 16:16:47 +0800

    commit homework

--- src/main/java/practice2/Receipt.java
+++ src/main/java/practice2/Receipt.java
@@ -4,32 +4,22 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class Receipt {
+    private BigDecimal tax;
 
     public Receipt() {
         tax = new BigDecimal(0.1);
         tax = tax.setScale(2, BigDecimal.ROUND_HALF_UP);
     }
 
-    private BigDecimal tax;
-
-    public double CalculateGrandTotal(List<Product> products, List<OrderItem> items) {
-        BigDecimal subTotal = calculateSubtotal(products, items);
-
+    private BigDecimal calculateSubtotal(List<Product> products, List<OrderItem> items) {
+        BigDecimal subTotal = new BigDecimal(0);
         for (Product product : products) {
-            OrderItem curItem = findOrderItemByProduct(items, product);
-
-            BigDecimal reducedPrice = product.getPrice()
-                    .multiply(product.getDiscountRate())
-                    .multiply(new BigDecimal(curItem.getCount()));
-
-            subTotal = subTotal.subtract(reducedPrice);
+            OrderItem item = findOrderItemByProduct(items, product);
+            BigDecimal itemTotal = product.getPrice().multiply(new BigDecimal(item.getCount()));
+            subTotal = subTotal.add(itemTotal);
         }
-        BigDecimal taxTotal = subTotal.multiply(tax);
-        BigDecimal grandTotal = subTotal.add(taxTotal);
-
-        return grandTotal.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+        return subTotal;
     }
-
 
     private OrderItem findOrderItemByProduct(List<OrderItem> items, Product product) {
         OrderItem curItem = null;
@@ -42,13 +32,29 @@ public class Receipt {
         return curItem;
     }
 
-    private BigDecimal calculateSubtotal(List<Product> products, List<OrderItem> items) {
-        BigDecimal subTotal = new BigDecimal(0);
+    private BigDecimal calculateReducedPrice(List<Product> products, List<OrderItem> items) {
+        BigDecimal reducedPrice = new BigDecimal(0);
+
         for (Product product : products) {
-            OrderItem item = findOrderItemByProduct(items, product);
-            BigDecimal itemTotal = product.getPrice().multiply(new BigDecimal(item.getCount()));
-            subTotal = subTotal.add(itemTotal);
+            OrderItem curItem = findOrderItemByProduct(items, product);
+
+            reducedPrice = reducedPrice.add(product.getPrice()
+                    .multiply(product.getDiscountRate())
+                    .multiply(new BigDecimal(curItem.getCount())));
         }
-        return subTotal;
+
+        return reducedPrice;
+    }
+
+    public double CalculateGrandTotal(List<Product> products, List<OrderItem> items) {
+        BigDecimal subTotal = calculateSubtotal(products, items);
+
+        BigDecimal reducedPrice = calculateReducedPrice(products, items);
+        subTotal = subTotal.subtract(reducedPrice);
+
+        BigDecimal taxTotal = subTotal.multiply(tax);
+        BigDecimal grandTotal = subTotal.add(taxTotal);
+
+        return grandTotal.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 }
--- src/main/java/practice3/PriceCaculator.java
+++ src/main/java/practice3/PriceCaculator.java
@@ -1,4 +1,54 @@
 package practice3;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class PriceCaculator {
+
+    private List<OrderLineItem> orderLineItemList;
+    private List<BigDecimal> discounts;
+    private BigDecimal tax;
+
+    public PriceCaculator(List<OrderLineItem> orderLineItemList, List<BigDecimal> discounts, BigDecimal tax) {
+        this.orderLineItemList = orderLineItemList;
+        this.discounts = discounts;
+        this.tax = tax;
+    }
+
+    private BigDecimal calcTotal()
+    {
+        BigDecimal subTotal = new BigDecimal(0);
+        for (OrderLineItem lineItem : orderLineItemList) {
+            subTotal = subTotal.add(lineItem.getPrice());
+        }
+
+        return subTotal;
+    }
+
+    private BigDecimal calcReduce()
+    {
+        BigDecimal subTotal = new BigDecimal(0);
+        for (BigDecimal discount : discounts) {
+            subTotal = subTotal.add(discount);
+        }
+
+        return subTotal;
+    }
+
+    private BigDecimal calcTax(BigDecimal subTotal)
+    {
+        return subTotal.multiply(tax);
+    }
+
+    public BigDecimal calculate() {
+        BigDecimal total = calcTotal();
+
+        BigDecimal reduce = calcReduce();
+        total = total.subtract(reduce);
+
+        BigDecimal tax = calcTax(total);
+        total = total.add(tax);
+
+        return total;
+    }
 }
